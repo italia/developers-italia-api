95785ae0d06fdac5187a7cbb66d87489f7094a66
Lorenzo <5399264+LorenzoS92@users.noreply.github.com>
2022-06-17 10:30:14 +0200

    feat(scaffolding-fiber): first scaffolding (#22)

---
+++ internal/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "github.com/gofiber/fiber/v2"
+
+func main() {
+	app := fiber.New(fiber.Config{
+		Prefork: true,
+	})
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+
+	err := app.Listen(":3000")
+	if err != nil {
+		return
+	}
+}
