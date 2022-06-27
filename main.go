a301e2369c07a8d0ed4c21e84f14c94c080dcd12
Lorenzo <5399264+LorenzoS92@users.noreply.github.com>
2022-06-27 18:57:07 +0200

    feat(limiter): Implement rate API limiter (#39)

--- main.go
+++ main.go
@@ -5,15 +5,26 @@ import (
 	"os"
 
 	"github.com/italia/developers-italia-api/internal/common"
+	"github.com/italia/developers-italia-api/internal/handlers"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/recover"
 
+	"github.com/caarlos0/env"
+	"github.com/gofiber/fiber/v2/middleware/limiter"
 	"github.com/italia/developers-italia-api/internal/database"
-	"github.com/italia/developers-italia-api/internal/handlers"
 )
 
+type Environment struct {
+	MaxRequests int `env:"MAX_REQUESTS" envDefault:"20"`
+}
+
 func main() {
+	environment := Environment{}
+	if err := env.Parse(&environment); err != nil {
+		log.Fatal(err)
+	}
+
 	if err := database.Init(os.Getenv("DATABASE_DSN")); err != nil {
 		log.Fatal(err)
 	}
@@ -25,6 +36,12 @@ func main() {
 
 	// Automatically recover panics in handlers
 	app.Use(recover.New())
+
+	// Use Fiber Rate API Limiter
+	app.Use(limiter.New(limiter.Config{
+		Max:               environment.MaxRequests,
+		LimiterMiddleware: limiter.SlidingWindow{},
+	}))
 
 	app.Get("/publishers", handlers.GetPublishers)
 	app.Get("/publishers/:id", handlers.GetPublisher)
