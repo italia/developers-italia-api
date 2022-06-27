04730a5dec980e93bba1396565ef2c72661b9866
Lorenzo Saraniti <lorenzo.saraniti@macaiapp.com>
2022-06-27 15:41:24 +0200

    feat(limiter): Implement rate API limiter

--- main.go
+++ main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"log"
 	"os"
+	"time"
 
 	"github.com/italia/developers-italia-api/internal/common"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/recover"
 
+	"github.com/gofiber/fiber/v2/middleware/limiter"
 	"github.com/italia/developers-italia-api/internal/database"
 	"github.com/italia/developers-italia-api/internal/handlers"
 )
@@ -25,6 +27,13 @@ func main() {
 
 	// Automatically recover panics in handlers
 	app.Use(recover.New())
+
+	// Use Fiber Rate API Limiter
+	app.Use(limiter.New(limiter.Config{
+		Max:               20,
+		Expiration:        30 * time.Second,
+		LimiterMiddleware: limiter.SlidingWindow{},
+	}))
 
 	app.Get("/publishers", handlers.GetPublishers)
 	app.Get("/publishers/:id", handlers.GetPublisher)
