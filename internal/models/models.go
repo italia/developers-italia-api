462eb240e1b720c1d3e37f21afec266482b78cb1
Lorenzo Saraniti <lorenzo.saraniti@macaiapp.com>
2022-07-08 12:44:12 +0200

    feat(onboarding): patch add multiple urls

--- internal/models/models.go
+++ internal/models/models.go
@@ -19,16 +19,16 @@ type Log struct {
 }
 
 type Publisher struct {
-	ID          string `gorm:"primarykey"`
-	Email       string `json:"email"`
-	Description string `json:"description"`
-	URL         []URL
-	CreatedAt   time.Time
-	UpdatedAt   time.Time
-	DeletedAt   gorm.DeletedAt `gorm:"index"`
+	ID           string         `gorm:"primarykey"`
+	Email        string         `json:"email"`
+	Description  string         `json:"description"`
+	URLAddresses []URLAddresses `gorm:"constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+	CreatedAt    time.Time
+	UpdatedAt    time.Time
+	DeletedAt    gorm.DeletedAt `gorm:"index"`
 }
 
-type URL struct {
+type URLAddresses struct {
 	gorm.Model
 	URL         string `json:"url"`
 	PublisherID string
