1ac4f8631c41c993f93598c1f3db2207702e0fe3
Fabio Bonelli <fb@fabiobonelli.it>
2022-07-18 20:28:11 +0200

    test: refactor, add fixtures support and logs tests (#75)

--- main_test.go
+++ main_test.go
@@ -1,113 +1,45 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 	"strings"
 	"testing"
 
+	"github.com/go-testfixtures/testfixtures/v3"
+	"github.com/gofiber/fiber/v2"
+	_ "github.com/mattn/go-sqlite3"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestEndpoints(t *testing.T) {
-	goodToken := "Bearer v2.local.TwwHUQEi8hr2Eo881_Bs5vK9dHOR5BgEU24QRf-U7VmUwI1yOEA6mFT0EsXioMkFT_T-jjrtIJ_Nv8f6hR6ifJXUOuzWEkm9Ijq1mqSjQatD3aDqKMyjjBA"
-	badToken := "Bearer v2.local.UngfrCDNwGUw4pff2oBNoyxYvOErcbVVqLndl6nzONafUCzktaOeMSmoI7B0h62zoxXXLqTm_Phl"
+var (
+	app       *fiber.App
+	db        *sql.DB
+	goodToken = "Bearer v2.local.TwwHUQEi8hr2Eo881_Bs5vK9dHOR5BgEU24QRf-U7VmUwI1yOEA6mFT0EsXioMkFT_T-jjrtIJ_Nv8f6hR6ifJXUOuzWEkm9Ijq1mqSjQatD3aDqKMyjjBA"
+	badToken  = "Bearer v2.local.UngfrCDNwGUw4pff2oBNoyxYvOErcbVVqLndl6nzONafUCzktaOeMSmoI7B0h62zoxXXLqTm_Phl"
+)
 
-	tests := []struct {
-		description string
+type TestCase struct {
+	description string
 
-		// Test input
-		route   string
-		method  string
-		body    string
-		headers map[string][]string
+	// Test input
+	fixtures []string
+	query    string
+	body     string
+	headers  map[string][]string
 
-		// Expected output
-		expectedCode        int
-		expectedBody        string
-		expectedContentType string
-		validateFunc        func(t *testing.T, data interface{})
-	}{
-		{
-			description: "publishers get route",
-			route:       "/v1/publishers",
-			method:      "GET",
+	// Expected output
+	expectedCode        int
+	expectedBody        string
+	expectedContentType string
+	validateFunc        func(t *testing.T, response map[string]interface{})
+}
 
-			expectedCode:        200,
-			expectedBody:        `{"data":[]}`,
-			expectedContentType: "application/json",
-		},
-		{
-			description: "non existing route",
-			route:       "/v1/i-dont-exist",
-			method:      "GET",
-
-			expectedCode:        404,
-			expectedBody:        `{"title":"Not Found","status":404}`,
-			expectedContentType: "application/problem+json",
-		},
-		{
-			description:  "publishers get non-existing id",
-			route:        "/v1/publishers/404",
-			method:       "GET",
-			expectedCode: 404,
-			expectedBody: `{"title":"can't get Publisher","detail":"Publisher was not found","status":404}`,
-
-			expectedContentType: "application/problem+json",
-		},
-		{
-			description:         "GET status",
-			route:               "/status",
-			method:              "GET",
-			expectedCode:        204,
-			expectedBody:        "",
-			expectedContentType: "",
-			// TODO: test cache headers
-		},
-
-		// Publishers
-		{
-			description: "POST publisher",
-			route:       "/v1/publishers",
-			method:      "POST",
-			body:        `{"URL":"https://www.example.com", "email":"example@example.com"}`,
-			headers: map[string][]string{
-				"Authorization": {goodToken},
-				"Content-Type":  {"application/json"},
-			},
-			expectedCode: 200,
-			validateFunc: func(t *testing.T, data interface{}) {
-				f, ok := data.(map[string]interface{})
-				if !ok {
-					t.Error(t, fmt.Errorf("expected map[string]interface{}, got %T", data))
-				}
-				fMap := f["data"].(map[string]interface{})
-				codeHost := fMap["codeHosting"].([]interface{})
-				assert.Equal(t, 1, len(codeHost))
-				codeHostElement := codeHost[0].(map[string]interface{})
-				assert.Equal(t, codeHostElement["url"], "https://www.example.com")
-				assert.Equal(t, fMap["email"], "example@example.com")
-			},
-			expectedContentType: "application/json",
-		},
-		{
-			description: "POST publisher - wrong token",
-			route:       "/v1/publishers",
-			method:      "POST",
-			body:        `{"name": "New publisher"}`,
-			headers: map[string][]string{
-				"Authorization": {badToken},
-				"Content-Type":  {"application/json"},
-			},
-			expectedCode:        401,
-			expectedBody:        `{"title":"token authentication failed","status":401}`,
-			expectedContentType: "application/problem+json",
-		},
-	}
-
+func init() {
 	os.Remove("./test.db")
 
 	os.Setenv("DATABASE_DSN", "file:./test.db")
@@ -116,14 +48,60 @@ func TestEndpoints(t *testing.T) {
 	// echo -n 'test-paseto-key-dont-use-in-prod'  | base64
 	os.Setenv("PASETO_KEY", "dGVzdC1wYXNldG8ta2V5LWRvbnQtdXNlLWluLXByb2Q=")
 
-	// Setup the app as it is done in the main function
-	app := Setup()
+	var err error
+	db, err = sql.Open("sqlite3", os.Getenv("DATABASE_DSN"))
+	if err != nil {
+		log.Fatal(err)
+	}
 
+	// This is needed, otherwise we get a database-locked error
+	// TODO: investigate the root cause
+	db.Exec("PRAGMA journal_mode=WAL;")
+
+	// Setup the app as it is done in the main function
+	app = Setup()
+}
+
+func TestMain(m *testing.M) {
+	code := m.Run()
+
+	os.Exit(code)
+}
+
+func loadFixtures(t *testing.T, files ...string) {
+	var filesWithPath []string
+	for _, file := range files {
+		filesWithPath = append(filesWithPath, "test/testdata/fixtures/"+file)
+	}
+
+	fixtures, err := testfixtures.New(
+		testfixtures.Database(db),
+		testfixtures.Dialect("sqlite"),
+		testfixtures.Files(filesWithPath...),
+	)
+	assert.Nil(t, err)
+
+	err = fixtures.Load()
+	assert.Nil(t, err)
+}
+
+func runTestCases(t *testing.T, tests []TestCase) {
 	for _, test := range tests {
-		t.Run(test.description, func(t *testing.T) {
+		description := test.description
+		if description == "" {
+			description = test.query
+		}
+
+		t.Run(description, func(t *testing.T) {
+			if len(test.fixtures) > 0 {
+				loadFixtures(t, test.fixtures...)
+			}
+
+			query := strings.Split(test.query, " ")
+
 			req, _ := http.NewRequest(
-				test.method,
-				test.route,
+				query[0],
+				query[1],
 				strings.NewReader(test.body),
 			)
 			if test.headers != nil {
@@ -140,9 +118,10 @@ func TestEndpoints(t *testing.T) {
 			assert.Nil(t, err)
 
 			if test.validateFunc != nil {
-				var bodyMap interface{}
+				var bodyMap map[string]interface{}
 				err = json.Unmarshal(body, &bodyMap)
 				assert.Nil(t, err)
+
 				test.validateFunc(t, bodyMap)
 			}
 
@@ -153,4 +132,163 @@ func TestEndpoints(t *testing.T) {
 			assert.Equal(t, test.expectedContentType, res.Header.Get("Content-Type"))
 		})
 	}
+}
+
+func TestApi(t *testing.T) {
+	tests := []TestCase{
+		{
+			description: "non existing route",
+			query:       "GET /v1/i-dont-exist",
+
+			expectedCode:        404,
+			expectedBody:        `{"title":"Not Found","status":404}`,
+			expectedContentType: "application/problem+json",
+		},
+	}
+
+	runTestCases(t, tests)
+}
+
+func TestPublishersEndpoints(t *testing.T) {
+	tests := []TestCase{
+		{
+			query: "GET /v1/publishers",
+
+			expectedCode:        200,
+			expectedBody:        `{"data":[]}`,
+			expectedContentType: "application/json",
+		},
+		{
+			description:  "publishers get non-existing id",
+			query:        "GET /v1/publishers/404",
+			expectedCode: 404,
+			expectedBody: `{"title":"can't get Publisher","detail":"Publisher was not found","status":404}`,
+
+			expectedContentType: "application/problem+json",
+		},
+		{
+			query: "POST /v1/publishers",
+			body:  `{"URL":"https://www.example.com", "email":"example@example.com"}`,
+			headers: map[string][]string{
+				"Authorization": {goodToken},
+				"Content-Type":  {"application/json"},
+			},
+			expectedCode: 200,
+			validateFunc: func(t *testing.T, response map[string]interface{}) {
+				fMap := response["data"].(map[string]interface{})
+				codeHost := fMap["codeHosting"].([]interface{})
+				assert.Equal(t, 1, len(codeHost))
+				codeHostElement := codeHost[0].(map[string]interface{})
+				assert.Equal(t, codeHostElement["url"], "https://www.example.com")
+				assert.Equal(t, fMap["email"], "example@example.com")
+			},
+			expectedContentType: "application/json",
+		},
+		{
+			description: "POST publisher - wrong token",
+			query:       "POST /v1/publishers",
+			body:        `{"name": "New publisher"}`,
+			headers: map[string][]string{
+				"Authorization": {badToken},
+				"Content-Type":  {"application/json"},
+			},
+			expectedCode:        401,
+			expectedBody:        `{"title":"token authentication failed","status":401}`,
+			expectedContentType: "application/problem+json",
+		},
+	}
+
+	runTestCases(t, tests)
+}
+
+func TestX(t *testing.T) {
+	tests := []TestCase{
+		{
+			query:    "GET /v1/logs",
+			fixtures: []string{"logs.yml"},
+
+			expectedCode:        200,
+			expectedContentType: "application/json",
+			validateFunc: func(t *testing.T, response map[string]interface{}) {
+				data := response["data"].([]interface{})
+
+				assert.Equal(t, 10, len(data))
+			},
+		},
+	}
+	runTestCases(t, tests)
+}
+
+func TestLogsEndpoints(t *testing.T) {
+	tests := []TestCase{
+		{
+			query:    "GET /v1/logs",
+			fixtures: []string{"logs.yml"},
+
+			expectedCode:        200,
+			expectedContentType: "application/json",
+			validateFunc: func(t *testing.T, response map[string]interface{}) {
+				data := response["data"].([]interface{})
+
+				assert.Equal(t, 10, len(data))
+			},
+		},
+		{
+			query:    "GET /v1/logs",
+			fixtures: []string{"logs.yml"},
+
+			expectedCode:        200,
+			expectedContentType: "application/json",
+			validateFunc: func(t *testing.T, response map[string]interface{}) {
+				data := response["data"].([]interface{})
+
+				assert.Equal(t, 10, len(data))
+			},
+		},
+		{
+			query:        "GET /v1/logs/eea19c82-0449-11ed-bd84-d8bbc146d165",
+			expectedCode: 404,
+			expectedBody: `{"title":"can't get Log","detail":"Log was not found","status":404}`,
+
+			expectedContentType: "application/problem+json",
+		},
+		{
+			query: "POST /v1/logs",
+			body:  `{"message": "New log"}`,
+			headers: map[string][]string{
+				"Authorization": {goodToken},
+				"Content-Type":  {"application/json"},
+			},
+			expectedCode:        200,
+			expectedContentType: "application/json",
+		},
+		{
+			description: "POST log - wrong token",
+			query:       "POST /v1/logs",
+			body:        `{"message": "New log"}`,
+			headers: map[string][]string{
+				"Authorization": {badToken},
+				"Content-Type":  {"application/json"},
+			},
+			expectedCode:        401,
+			expectedBody:        `{"title":"token authentication failed","status":401}`,
+			expectedContentType: "application/problem+json",
+		},
+	}
+
+	runTestCases(t, tests)
+}
+
+func TestStatusEndpoints(t *testing.T) {
+	tests := []TestCase{
+		{
+			query:               "GET /status",
+			expectedCode:        204,
+			expectedBody:        "",
+			expectedContentType: "",
+			// TODO: test cache headers
+		},
+	}
+
+	runTestCases(t, tests)
 }
