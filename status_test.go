13f0718c883f89501c24a2b2d17ca1cbaa00f68d
Fabio Bonelli <fb@fabiobonelli.it>
2026-03-20 20:53:23 +0100

    feat: add healthcheck middleware (#295)
    /livez and /readyz can be used by k8s to tell liveness (process alive)
    from readiness (DB reachable).

--- status_test.go
+++ status_test.go
@@ -4,8 +4,20 @@ import (
 	"testing"
 )
 
-func TestStatusEndpoints(t *testing.T) {
+func TestHealthEndpoints(t *testing.T) {
 	tests := []TestCase{
+		{
+			query:               "GET /livez",
+			expectedCode:        200,
+			expectedBody:        "OK",
+			expectedContentType: "text/plain; charset=utf-8",
+		},
+		{
+			query:               "GET /readyz",
+			expectedCode:        200,
+			expectedBody:        "OK",
+			expectedContentType: "text/plain; charset=utf-8",
+		},
 		{
 			query:               "GET /v1/status",
 			expectedCode:        204,
